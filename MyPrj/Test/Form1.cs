9ab2f3f4a9a08a3734861fde06a4460fe57d0e42
lxl <lxl177177@163.com>
2020-12-24 10:15:05 +0800

    1.获取内存

--- MyPrj/Test/Form1.cs
+++ MyPrj/Test/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,53 @@ namespace Test
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int memSize = GetPhisicalMemory();
+
+        }
+
+
+
+        /// <summary>
+        /// 获取系统内存大小
+        /// 以G为单位
+        /// </summary>
+        /// <returns>内存大小（单位M）</returns>
+        private int GetPhisicalMemory()
+        {
+            int phyMemSize = 0;
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher();   //用于查询一些如系统信息的管理对象 
+                searcher.Query = new SelectQuery("Win32_PhysicalMemory ", "", new string[] { "Capacity" });//设置查询条件 
+                ManagementObjectCollection collection = searcher.Get();   //获取内存容量 
+                ManagementObjectCollection.ManagementObjectEnumerator em = collection.GetEnumerator();
+                long capacity = 0;
+                while (em.MoveNext())
+                {
+                    ManagementBaseObject baseObj = em.Current;
+                    if (baseObj.Properties["Capacity"].Value != null)
+                    {
+                        try
+                        {
+                            capacity += long.Parse(baseObj.Properties["Capacity"].Value.ToString());
+                        }
+                        catch
+                        {
+                            return 0;
+                        }
+                    }
+                }
+                phyMemSize = (int)(capacity / 1024 / 1024 / 1024);
+            }
+            catch (Exception)
+            {
+            }
+            return phyMemSize;
         }
     }
 }
