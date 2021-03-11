2a7d4dbd9d99e85cda99c10f4c9360968d7be3f6
lxl <lxl177177@163.com>
2021-03-11 17:17:58 +0800

    1.维持状态插件。

---
+++ MyPrj/VPNTool/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace VPNTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.urlTxt = new System.Windows.Forms.TextBox();
+            this.navBtn = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 48);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1011, 530);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.webBrowser1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1003, 504);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "浏览";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(997, 498);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1003, 504);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "操作日志";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(997, 498);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // urlTxt
+            // 
+            this.urlTxt.Location = new System.Drawing.Point(4, 10);
+            this.urlTxt.Name = "urlTxt";
+            this.urlTxt.Size = new System.Drawing.Size(740, 21);
+            this.urlTxt.TabIndex = 1;
+            // 
+            // navBtn
+            // 
+            this.navBtn.Location = new System.Drawing.Point(764, 10);
+            this.navBtn.Name = "navBtn";
+            this.navBtn.Size = new System.Drawing.Size(236, 23);
+            this.navBtn.TabIndex = 2;
+            this.navBtn.Text = "进入";
+            this.navBtn.UseVisualStyleBackColor = true;
+            this.navBtn.Click += new System.EventHandler(this.navBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 583);
+            this.Controls.Add(this.navBtn);
+            this.Controls.Add(this.urlTxt);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "人寿VPN定时工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox urlTxt;
+        private System.Windows.Forms.Button navBtn;
+    }
+}
+
---
+++ MyPrj/VPNTool/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPNTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            reLoadTimer.Interval = 120000;
+            reLoadTimer.Elapsed += ReLoadTimer_Elapsed;
+            reLoadTimer.Start();
+
+        }
+
+        private void ReLoadTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                this.webBrowser1.Navigate("https://www.baidu.com/");
+                Thread.Sleep(10000);
+                string checkUrl = "http://9.0.9.11/workbench/workbench/login.html";
+                String confUrl = ConfigurationManager.AppSettings["url"];
+                if (!String.IsNullOrEmpty(confUrl)) { checkUrl = confUrl; }
+                this.webBrowser1.Navigate(checkUrl);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        #region timer
+
+        /// <summary>
+        /// 重新加载定时器
+        /// </summary>
+        System.Timers.Timer reLoadTimer = new System.Timers.Timer();
+
+
+        #endregion
+
+        /// <summary>
+        /// 手动进入
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void navBtn_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(this.urlTxt.Text) && !String.IsNullOrEmpty(this.urlTxt.Text.Trim()))
+            {
+                this.webBrowser1.Navigate(this.urlTxt.Text.Trim());
+            }
+        }
+    }
+}
